9294eadbb4b92bfdfdfa9aaf6c0eca5ca0f7e013
Tim Anema <timanema@gmail.com>
2026-09-06 20:38:22 -0400

    Silence logging in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,17 @@ require "bundler/setup"
 require "stir_fry"
 require_relative "fixtures/button"
 
+# Keep the request/server logs out of the spec output. Both the memoized
+# StirFry.logger and the one StirFry.run! builds itself funnel through
+# default_logger, so overriding it here silences every code path.
+module SilentLogger
+  def default_logger(klass, _options = {})
+    name = klass.respond_to?(:name) ? klass.name.to_s : klass.to_s
+    SemanticLogger[name]
+  end
+end
+StirFry.singleton_class.prepend(SilentLogger)
+
 RSpec.configure do |config|
   config.disable_monkey_patching!
   config.filter_run_when_matching :focus
